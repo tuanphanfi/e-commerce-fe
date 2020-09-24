69b9fba19eb70692cae29660dd341f812a7d7172
tuanphanfi <tuanphan.fi@gmail.com>
2020-09-24 18:51:06 +0700

    h

--- src/redux/api.js
+++ src/redux/api.js
@@ -3,7 +3,7 @@ import store from "./store";
 import { alertActions } from "./actions/alert.actions";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
   },
